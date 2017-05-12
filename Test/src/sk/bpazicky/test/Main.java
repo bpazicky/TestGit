e2d0773b1f26a3ae3bdf111eaf1f1090ceb6945e
Ja <Ja@Ja-PC>
2017-05-12 22:44:58 +0200

    Druhý commit

--- Test/src/sk/bpazicky/test/Main.java
+++ Test/src/sk/bpazicky/test/Main.java
@@ -3,6 +3,8 @@ package sk.bpazicky.test;
 public class Main {
 
 	public static void main(String[] args) {
+		//Uvidime ci to funguuje
+		//som celkovy zvedavý ale vyzerá že to pôjde.
 		System.out.println("Funguje to !!");
 	}
 
